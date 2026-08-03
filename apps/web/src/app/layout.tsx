eed949220333df91effe2c8f3dca94b0d2491987
adajala <ademola2993k@gmail.com>
2026-08-03 16:00:25 +0100

    style: remove default background styling from root layout

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,10 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Nav } from "@/components/nav";
-
-import localFont from "next/font/local";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
