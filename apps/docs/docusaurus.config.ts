79a42f2fbfeefe6fe3b79c9000201e0772db8f20
wagmiiii <ademola2993k@gmail.com>
2026-08-13 19:27:38 +0100

    docs: configure docusaurus for github pages

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://accensa.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/accensa-app/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'accensa', // Usually your GitHub org/user name.
+  projectName: 'accensa-app', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
